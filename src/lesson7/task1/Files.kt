95ed9860bc5332795818caa3b6d7e49dc89190ac
MargarTuc <theiraesi@gmail.com>
2018-12-19 04:06:27 +0300

    Lesson

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -32,8 +32,7 @@ fun alignFile(inputName: String, lineLength: Int, outputName: String) {
                 if (word.length + currentLineLength >= lineLength) {
                     outputStream.newLine()
                     currentLineLength = 0
-                }
-                else {
+                } else {
                     outputStream.write(" ")
                     currentLineLength++
                 }
@@ -243,15 +242,15 @@ Suspendisse ~~et elit in enim tempus iaculis~~.
  *
  * Соответствующий выходной файл:
 <html>
-    <body>
-        <p>
-            Lorem ipsum <i>dolor sit amet</i>, consectetur <b>adipiscing</b> elit.
-            Vestibulum lobortis. <s>Est vehicula rutrum <i>suscipit</i></s>, ipsum <s>lib</s>ero <i>placerat <b>tortor</b></i>.
-        </p>
-        <p>
-            Suspendisse <s>et elit in enim tempus iaculis</s>.
-        </p>
-    </body>
+<body>
+<p>
+Lorem ipsum <i>dolor sit amet</i>, consectetur <b>adipiscing</b> elit.
+Vestibulum lobortis. <s>Est vehicula rutrum <i>suscipit</i></s>, ipsum <s>lib</s>ero <i>placerat <b>tortor</b></i>.
+</p>
+<p>
+Suspendisse <s>et elit in enim tempus iaculis</s>.
+</p>
+</body>
 </html>
  *
  * (Отступы и переносы строк в примере добавлены для наглядности, при решении задачи их реализовывать не обязательно)
@@ -294,67 +293,135 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
  *
  * Пример входного файла:
 ///////////////////////////////начало файла/////////////////////////////////////////////////////////////////////////////
-* Утка по-пекински
-    * Утка
-    * Соус
-* Салат Оливье
-    1. Мясо
-        * Или колбаса
-    2. Майонез
-    3. Картофель
-    4. Что-то там ещё
-* Помидоры
-* Фрукты
-    1. Бананы
-    23. Яблоки
-        1. Красные
-        2. Зелёные
+ * Утка по-пекински
+ * Утка
+ * Соус
+ * Салат Оливье
+1. Мясо
+ * Или колбаса
+2. Майонез
+3. Картофель
+4. Что-то там ещё
+ * Помидоры
+ * Фрукты
+1. Бананы
+23. Яблоки
+1. Красные
+2. Зелёные
 ///////////////////////////////конец файла//////////////////////////////////////////////////////////////////////////////
  *
  *
  * Соответствующий выходной файл:
 ///////////////////////////////начало файла/////////////////////////////////////////////////////////////////////////////
 <html>
-  <body>
-    <ul>
-      <li>
-        Утка по-пекински
-        <ul>
-          <li>Утка</li>
-          <li>Соус</li>
-        </ul>
-      </li>
-      <li>
-        Салат Оливье
-        <ol>
-          <li>Мясо
-            <ul>
-              <li>
-                  Или колбаса
-              </li>
-            </ul>
-          </li>
-          <li>Майонез</li>
-          <li>Картофель</li>
-          <li>Что-то там ещё</li>
-        </ol>
-      </li>
-      <li>Помидоры</li>
-      <li>
-        Яблоки
-        <ol>
-          <li>Красные</li>
-          <li>Зелёные</li>
-        </ol>
-      </li>
-    </ul>
-  </body>
+<body>
+<ul>
+<li>
+Утка по-пекински
+<ul>
+<li>Утка</li>
+<li>Соус</li>
+</ul>
+</li>
+<li>
+Салат Оливье
+<ol>
+<li>Мясо
+<ul>
+<li>
+Или колбаса
+</li>
+</ul>
+</li>
+<li>Майонез</li>
+<li>Картофель</li>
+<li>Что-то там ещё</li>
+</ol>
+</li>
+<li>Помидоры</li>
+<li>
+Яблоки
+<ol>
+<li>Красные</li>
+<li>Зелёные</li>
+</ol>
+</li>
+</ul>
+</body>
 </html>
 ///////////////////////////////конец файла//////////////////////////////////////////////////////////////////////////////
  * (Отступы и переносы строк в примере добавлены для наглядности, при решении задачи их реализовывать не обязательно)
  */
 fun markdownToHtmlLists(inputName: String, outputName: String) {
-    TODO()
+    val writer = File(outputName).bufferedWriter()
+    writer.write("<html>\n<body>\n")
+    val oldytut = File(inputName).readLines().toMutableList()
+    fun labuda(list: MutableList<String>): String {
+        val num = listOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+        val build = StringBuilder()
+        var i = 0
+        when (list[0].first()) {
+            '*' -> build.append("<ul>\n")
+            in num -> build.append("<ol>\n")
+        }
+        while (i < list.size) {
+            when {
+                list[i].first() == '*' -> {
+                    if (list[i + 1].first() != ' ') {
+                        build.append("<li>" + list[i].substring(2) + "</li>\n")
+                    }
+                    if (list[i + 1].first() == ' ') {
+                        build.append("<li>" + list[i].substring(2))
+                        var temp = i + 1
+                        while (list[temp].first() == ' ') temp++
+                        val temp2 = mutableListOf<String>()
+                        for (a in (i + 1)..(temp - 1)) temp2.add(list[a])
+                        var c = 0
+                        while (c < temp2.size) {
+                            temp2[c] = temp2[c].substring(4)
+                            c++
+                        }
+                        temp2.add("!")
+                        build.append(labuda(temp2))
+                        i += temp2.size - 1
+                        build.append("</li>\n")
+                    }
+                }
+                list[i].first() in num -> {
+                    if (list[i + 1].first() != ' ') {
+                        build.append("<li>" + list[i].substring(3) + "</li>\n")
+                    }
+                    if (list[i + 1].first() == ' ') {
+                        build.append("<li>" + list[i].substring(3))
+                        var temp = i + 1
+                        while (list[temp].first() == ' ') temp++
+                        val temp2 = mutableListOf<String>()
+                        for (a in (i + 1)..(temp - 1)) temp2.add(list[a])
+                        var c = 0
+                        while (c < temp2.size) {
+                            temp2[c] = temp2[c].substring(4)
+                            c++
+                        }
+                        temp2.add("!")
+                        build.append(labuda(temp2))
+                        i += temp2.size - 1
+                        build.append("</li>\n")
+                    }
+                }
+            }
+            i++
+            if (list[i] == "!") break
+        }
+        when (list[0].first()) {
+            '*' -> build.append("</ul>\n")
+            in num -> build.append("</ol>\n")
+        }
+        return build.toString()
+    }
+    oldytut.add("!")
+    writer.write(labuda(oldytut))
+    writer.write("</body>\n</html>")
+    writer.close()
 }
 
 /**
@@ -375,23 +442,23 @@ fun markdownToHtml(inputName: String, outputName: String) {
  * Вывести в выходной файл процесс умножения столбиком числа lhv (> 0) на число rhv (> 0).
  *
  * Пример (для lhv == 19935, rhv == 111):
-   19935
-*    111
+19935
+ *    111
 --------
-   19935
+19935
 + 19935
 +19935
 --------
- 2212785
+2212785
  * Используемые пробелы, отступы и дефисы должны в точности соответствовать примеру.
  * Нули в множителе обрабатывать так же, как и остальные цифры:
-  235
-*  10
+235
+ *  10
 -----
-    0
+0
 +235
 -----
- 2350
+2350
  *
  */
 fun printMultiplicationProcess(lhv: Int, rhv: Int, outputName: String) {
@@ -405,16 +472,16 @@ fun printMultiplicationProcess(lhv: Int, rhv: Int, outputName: String) {
  * Вывести в выходной файл процесс деления столбиком числа lhv (> 0) на число rhv (> 0).
  *
  * Пример (для lhv == 19935, rhv == 22):
-  19935 | 22
- -198     906
- ----
-    13
-    -0
-    --
-    135
-   -132
-   ----
-      3
+19935 | 22
+-198     906
+----
+13
+-0
+--
+135
+-132
+----
+3
 
  * Используемые пробелы, отступы и дефисы должны в точности соответствовать примеру.
  *
